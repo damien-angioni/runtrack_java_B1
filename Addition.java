83d14f112be8183a3df7acbb400b9f98b3f78180
damien-angioni <damien.angioni@laplateforme.io>
2023-05-09 14:57:47 +0200

    completed: Job 08, Day 01

---
+++ Addition.java
@@ -0,0 +1,7 @@
+public class Addition {
+    public static void main(String[] args) {
+        int diviseur=73,dividende=3;
+        double res=(double)diviseur/dividende;
+        System.out.printf("Le résultat de la division est = "+ res + "\n");
+    }
+}
