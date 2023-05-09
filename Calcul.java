a72efa421639348a004de7e6aeb2e9b7f99943de
damien-angioni <damien.angioni@laplateforme.io>
2023-05-09 13:37:27 +0200

    completed: Job 05, Day 01

---
+++ Calcul.java
@@ -0,0 +1,14 @@
+public class Calcul {
+    static void Main() {                        //Main method
+        int nombre = 7;                         //Defining variable
+        System.out.println(nombre);             //Printing variable
+        nombre=nombre+3;                        //Processing Variable
+        System.out.println(nombre);
+        nombre=nombre*5;
+        System.out.println(nombre);
+    }
+
+    public static void main(String[] args) {    //Calling Main Method
+        Main();
+    }
+}
