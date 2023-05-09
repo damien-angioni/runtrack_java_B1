2b69ccc66bcf80c30d63725ea16234e305d2ae77
damien-angioni <damien.angioni@laplateforme.io>
2023-05-09 12:39:20 +0200

    completed: Job 03, Day 01

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//Runtrack "Hello World" program
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("Hello World !");
+        /*
+        This program displays "Hello World"! in the shell.
+        It does so using 'printf' system function.
+         */
+    }
+}
