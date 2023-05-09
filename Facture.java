bce9315b5e69910067476e23e3364e122665d74b
damien-angioni <damien.angioni@laplateforme.io>
2023-05-09 15:47:35 +0200

    completed: Job 08, Day 01

--- Facture.java
+++ Facture.java
@@ -1,5 +1,19 @@
+import java.text.DecimalFormat;                                         //Import library to format decimal variable
+
 public class Facture {
     public static void main(String[] args) {
-        
+
+        DecimalFormat df = new DecimalFormat("0.00");           //Set the format pattern
+
+        double prix=49.99;
+        int quantité=3;
+        double tarifHT=prix*quantité;
+        double tva=(double) (20*tarifHT)/100;
+        double tarifTTC=tarifHT+tva;
+        System.out.printf("Le prix: "+ prix + "\n");
+        System.out.printf("La quantité: "+ quantité + "\n");
+        System.out.printf("Total HT: "+ df.format(tarifHT) + "\n");     //Format decimal displayed
+        System.out.printf("TVA: "+ df.format(tva) + "\n");              //Format decimal displayed
+        System.out.printf("Total TTC "+ df.format(tarifTTC) + "\n");    //Format decimal displayed
     }
 }
