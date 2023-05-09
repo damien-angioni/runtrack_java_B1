971ead9ed95be068518e1c775aa20bf303c75df2
damien-angioni <damien.angioni@laplateforme.io>
2023-05-09 16:08:59 +0200

    completed: Job 09, Day 01

---
+++ MyString.java
@@ -0,0 +1,10 @@
+public class MyString {
+    public static void main(String[] args) {
+        String str1 = "level 1";
+        String str2 = "level 2", str3 = "level 3";
+        String str4;
+        str4 = "level 4";
+        String str5 = System.getProperty("user.name");
+        System.out.println(str1 + " " + str2 + " " + str3 + " " + str4 + " " + str5);
+    }
+}
