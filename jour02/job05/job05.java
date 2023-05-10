e61ae4a73e7a8fc9dfa234d6bc323a0e97e0df26
damien-angioni <damien.angioni@laplateforme.io>
2023-05-10 15:32:01 +0200

    day 2 job05

--- jour02/job05/job05.java
+++ jour02/job05/job05.java
@@ -1,9 +1,28 @@
 package jour02.job05;
 
+import java.util.Scanner;
+
 public class job05 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100 ; i=i+2) {
-            System.out.println(i);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("votre âge: ");
+        String nb1 = sc.nextLine();
+        try {
+            int intnb1 = Integer.parseInt(nb1);
+            if (16< intnb1 &&  intnb1 <67){
+                if(intnb1>55){
+                    System.out.println("Il sera difficile de trouver un emploi!");
+                }
+                else{
+                    System.out.println("Vous avez l'age pour travailler!");
+                }
+            }
+            else{
+                System.out.println("Vous n'avez pas l'age pour travailler!");
+            }
+        }
+        catch (NumberFormatException ex) {
+            ex.printStackTrace();
         }
     }
 }
