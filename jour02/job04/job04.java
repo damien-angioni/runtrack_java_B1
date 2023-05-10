b7d7ca3731e8a6f3891a2b22586d392db18ea7c7
damien-angioni <damien.angioni@laplateforme.io>
2023-05-10 15:11:33 +0200

    day 2 job 4

---
+++ jour02/job04/job04.java
@@ -0,0 +1,9 @@
+package jour02.job04;
+
+public class job04 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100 ; i=i+2) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ jour02/job05/job05.java
@@ -0,0 +1,9 @@
+package jour02.job05;
+
+public class job05 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100 ; i=i+2) {
+            System.out.println(i);
+        }
+    }
+}
