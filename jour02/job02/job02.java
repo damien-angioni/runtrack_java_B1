23004963783f04a39ed2bc9bba0c03f6d28cfc6a
damien-angioni <damien.angioni@laplateforme.io>
2023-05-10 14:44:04 +0200

    Day 2 Job1 &2
    -Finished job1
    -Finished job 2

---
+++ jour02/job02/job02.java
@@ -0,0 +1,9 @@
+package jour02.job02;
+
+public class job02 {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 0 ; i--) {
+            System.out.println(i);
+        }
+    }
+}
